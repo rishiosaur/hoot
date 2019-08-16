78ac2e040c8d07218439ff159a6b3577b3e69744
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-16 13:16:45 -0400

    Update bin

---
+++ bin/assignments/makeAssignment.js
@@ -0,0 +1,108 @@
+const chalk = require("chalk")
+const shell = require("shelljs")
+
+async function makeAssignment(name) {
+    if (!shell.which("git")) {
+      shell.echo("Sorry, this script requires git");
+      shell.exit(1);
+    }
+    let verification = await verifySchool();
+    let subjects = await getSubjects();
+    if (!subjects) {
+      console.log(
+        chalk.red("ERROR ") +
+          chalk.blue("You do not have subjects. Try running ") +
+          chalk.green("hoot subject <title>")
+      );
+      return;
+    }
+  
+    if (!verification) {
+      console.log(chalk.red("ERROR: ") + chalk.blue("School not found"));
+      console.log(chalk.green("Try running ") + chalk.blue("hoot setup"));
+      return;
+    }
+    console.log(
+      chalk.green(`Alright, let's make your assignment called: ${name}`)
+    );
+    let answers = await inquirer.prompt([
+      {
+        type: "list",
+        name: "subject",
+        message: "Subject",
+        choices: subjects
+      },
+      {
+        type: "list",
+        name: "type",
+        message: "Assignment type",
+        choices: ["Report", "Short-answer", "Presentation"]
+      },
+      {
+        type: "confirm",
+        name: "research",
+        message: "Add research folder",
+        default: true
+      }
+    ]);
+  
+    let aVer = await verifyAssignment(name, answers.subject);
+    if (aVer) {
+      console.log(
+        chalk.red("ERROR ") + chalk.blue("Assignment already exists on file.")
+      );
+      return;
+    }
+  
+    await mkdirSync(
+      `/Users/${os.userInfo().username}/Documents/School/${
+        answers.subject
+      }/${name}`
+    );
+    console.log("Assignment folder created.");
+    await mkdirSync(
+      `/Users/${os.userInfo().username}/Documents/School/${
+        answers.subject
+      }/${name}/mark`
+    );
+    console.log("Rubric folder created.");
+    if (answers.research) {
+      await mkdirSync(
+        `/Users/${os.userInfo().username}/Documents/School/${
+          answers.subject
+        }/${name}/research`
+      );
+      console.log("Research folder created.");
+    }
+    await copy(
+      resolve(`./templates/${answers.type.toLowerCase()}`),
+      `/Users/${os.userInfo().username}/Documents/School/${
+        answers.subject
+      }/${name}/`,
+      function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Copied " + answers.type + " folder");
+          shell.cd(
+            `/Users/${os.userInfo().username}/Documents/School/${
+              answers.subject
+            }/${name}`
+          );
+          console.log("Changed directories to assignment");
+          console.log("Initializing Git")
+          if (shell.exec("git init").code !== 0) {
+            shell.echo("Error: Git commit failed");
+            shell.exit(1);
+          }
+          console.log("Installing NPM packages")
+          
+          if (shell.exec("npm install").code !== 0) {
+            shell.echo("Error: NPM install failed");
+            shell.exit(1);
+          }
+          
+        }
+      }
+    ); //copies directory, even if it has subdirectories or files
+  }
