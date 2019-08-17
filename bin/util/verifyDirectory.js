79430cf77245353dc46d61ff4474de8254d174ca
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-17 15:23:00 -0400

    Refactor verifyDirect

--- bin/util/verifyDirectory.js
+++ bin/util/verifyDirectory.js
@@ -5,15 +5,14 @@ const chalk  = require('chalk')
 
 async function verifyDirectory (subfolder, invert) {
     let path = getDirectoryPath(subfolder)
-    
     if (existsSync(path)) {
         return true
-    } else if(!invert) {
+    } else if(invert) {
+        return false
+    } else {
         console.log(chalk.red("ERROR: ") + chalk.blue(`${subfolder} not found.`));
         console.log(chalk.green("Try running ") + chalk.blue("hoot setup") + " or " + chalk.blue("hoot assignment"));
         shell.exit(1);
-        return false
-    } else {
         return false
     }
 }
