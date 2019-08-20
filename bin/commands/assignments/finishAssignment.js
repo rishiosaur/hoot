7c7c9c9b563d00cb27199b287bca9f982a6f64f8
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-20 12:18:28 -0400

    More scaffolding and tests

--- bin/commands/assignments/finishAssignment.js
+++ bin/commands/assignments/finishAssignment.js
@@ -3,10 +3,21 @@ const { getDirectory } = require("../../util/getDirectory")
 const { getDirectoryPath } = require("../../util/getDirectoryPath")
 const shell = require("shelljs")
 const { verifyDirectory } = require("../../util/verifyDirectory")
+const { makeDirectory } = require("../../util/makeDirectory")
 
-async function finishAssignment(path){
+async function finishAssignment(subject,assignment){
+    let path = `${subject}/${assignment}`
     await verifyDirectory(path)
-    console.log('hi')
+    await makeDirectory(`${path}/mark`)
+    let assignmentHootJSON = require(getDirectoryPath(`${path}/hoot.json`))
+    console.log(assignmentHootJSON)
+    // let answers = inquirer.prompt([
+    //     {
+    //         message: "What was your mark for this assignment?",
+    //         type: "number",
+            
+    //     }
+    // ])
 }
 
 module.exports = {
