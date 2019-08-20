43adaa5ab8999ee49bc1190eccc9cbdcac4edafc
Rishi Kothari <itsrishikothari@gmail.com>
2019-08-20 12:34:43 -0400

    Update for sync

--- bin/commands/assignments/finishAssignment.js
+++ bin/commands/assignments/finishAssignment.js
@@ -1,23 +1,33 @@
 const inquirer = require("inquirer")
 const { getDirectory } = require("../../util/getDirectory")
 const { getDirectoryPath } = require("../../util/getDirectoryPath")
+const { writeFile, unlinkSync } = require("fs")
 const shell = require("shelljs")
 const { verifyDirectory } = require("../../util/verifyDirectory")
 const { makeDirectory } = require("../../util/makeDirectory")
 
 async function finishAssignment(subject,assignment){
     let path = `${subject}/${assignment}`
+    let pathToAssignmentJSON = getDirectoryPath(`${path}/hoot.json`)
     await verifyDirectory(path)
-    await makeDirectory(`${path}/mark`)
-    let assignmentHootJSON = require(getDirectoryPath(`${path}/hoot.json`))
-    console.log(assignmentHootJSON)
-    // let answers = inquirer.prompt([
-    //     {
-    //         message: "What was your mark for this assignment?",
-    //         type: "number",
-            
-    //     }
-    // ])
+    // await makeDirectory(`${path}/mark`)
+    let assignmentHootJSON = require(pathToAssignmentJSON)
+    
+    let answers = await inquirer.prompt([
+        {
+            message: "What was your mark for this assignment?",
+            type: "number",
+            name: "mark"
+        }
+    ])
+    assignmentHootJSON.mark = answers.mark
+    unlinkSync(pathToAssignmentJSON)
+    await writeFile(pathToAssignmentJSON, JSON.stringify(assignmentHootJSON), function (err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(assignmentHootJSON));
+        console.log('writing to ' + pathToAssignmentJSON);
+    });
+
 }
 
 module.exports = {
