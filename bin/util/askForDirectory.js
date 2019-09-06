8b189ac1c3db08b0032beaf95827ba2ab9ac488d
Rishi Kothari <itsrishikothari@gmail.com>
2019-09-06 19:26:21 -0400

    askForDirectory done

---
+++ bin/util/askForDirectory.js
@@ -0,0 +1,67 @@
+const { getDirectoryPath } = require("./getDirectoryPath");
+const { joinPath } = require("./joinPath");
+const { getDirectory } = require("./getDirectory");
+const shell = require("shelljs");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+
+async function askDirectoryName(level, use, choices) {
+  let answers = await inquirer.prompt({
+    type: "list",
+    name: "answer",
+    message: `What ${level} is this ${use} in?`,
+    choices: choices
+  });
+  return answers;
+}
+
+async function askForDirectory(level, use) {
+  let finalPath = [];
+
+  var termChoices, termChoice;
+  var subjectChoices, subjectChoice;
+  var unitChoices, unitChoice;
+
+  termChoices = await getDirectory("");
+  termChoice = await askDirectoryName("term", use, termChoices);
+  finalPath.push(termChoice.answer);
+
+  if (level > 1) {
+    subjectChoices = await getDirectory(termChoice.answer);
+
+    if (subjectChoices.length == 0) {
+      console.log(
+        chalk.red("ERROR ") +
+          chalk.blue("You do not have any subjects. Try running ") +
+          chalk.green("hoot subject")
+      );
+      shell.exit(1);
+    }
+
+    subjectChoice = await askDirectoryName("subject", use, subjectChoices);
+    finalPath.push(subjectChoice.answer);
+  }
+
+  if (level > 2) {
+    unitChoices = await getDirectory(
+      joinPath(termChoice.answer, subjectChoice.answer)
+    );
+
+    if (unitChoices.length == 0) {
+        console.log(
+          chalk.red("ERROR ") +
+            chalk.blue("You do not have any units. Try running ") +
+            chalk.green("hoot unit")
+        );
+        shell.exit(1);
+    }
+
+    unitChoice = await askDirectoryName("unit", use, subjectChoices);
+    finalPath.push(unitChoice.answer);
+  }
+  return joinPath(finalPath)
+}
+
+module.exports = {
+  askForDirectory: askForDirectory
+};
