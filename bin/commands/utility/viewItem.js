26ccc5f870e7161ac7780d708b57f13f3c51b2cd
Rishi Kothari <itsrishikothari@gmail.com>
2019-09-06 21:58:03 -0400

    Cleanup

--- bin/commands/utility/viewItem.js
+++ bin/commands/utility/viewItem.js
@@ -10,24 +10,26 @@ const chalk = require("chalk");
 async function viewItem() {
   let homedir = await os.homedir()
   console.log(`Format: ${chalk.blue("School")} / ${chalk.green("Term <number>")} / ${chalk.yellow("<Subject>")} / ${chalk.red("<Unit>")} / <Assignments|Finished>`)
-  let stuff = await shell.exec(`find ${getDirectoryPath("")} -type d  -not -path '*/\.*';`, {silent: true}).stdout.split("\n").slice(0,-1)
+  let stuff = await shell.exec(`find ${getDirectoryPath("").slice(0,-1)} -type d  -not -path '*/\.*';`, {silent: true}).stdout.split("\n").slice(0,-1)
   let formattedStuff = stuff.map(path => {
-    return path.replace(homedir + "/Documents", "")
+    let tempPath = path.replace(homedir + "/Documents", "").substring(1).split("/")
+    tempPath.map((folder, index) => {
+      if(index == 0){
+        tempPath[index] = chalk.blue(folder)
+      }
+      if(index == 1) {
+        tempPath[index] = chalk.green(folder)
+      }
+      if(index == 2) {
+        tempPath[index] = chalk.yellow(folder)
+      }
+      if(index == 3) {
+        tempPath[index] = chalk.red(folder)
+      }
+    })
+    console.log(tempPath.join("/"))
+    return tempPath
   })
-  console.log(formattedStuff)
-  // let terms = await getDirectory("");
-  //   console.log(chalk.blue("Your assignments:"))
-  //   terms.forEach((term, index) => (
-  //       getDirectory(term).then(subject => {
-  //           console.log(chalk.green(terms[index]))
-  //           subject.forEach(subject => {
-  //             console.log(chalk.blue(subject))
-  //             console.log(getDirectory(getDirectoryPath(joinPath([term,subject]))))
-  //           })
-  //           //Separator of the subjects
-  //           console.log("---")
-  //       })
-  //   ))
 
 }
 
