df2e25d4a75259371281db96056ed5a3b503595b
Rishi Kothari <itsrishikothari@gmail.com>
2019-09-06 22:01:54 -0400

    Formatting

--- bin/commands/utility/viewItem.js
+++ bin/commands/utility/viewItem.js
@@ -1,36 +1,28 @@
-const inquirer = require("inquirer");
-const { getDirectory } = require("../../util/getDirectory");
 const { getDirectoryPath } = require("../../util/getDirectoryPath");
-const os = require("os")
-const { joinPath } = require("../../util/joinPath")
+const os = require("os");
 
 const shell = require("shelljs");
 const chalk = require("chalk");
 
 async function viewItem() {
-  let homedir = await os.homedir()
-  console.log(`Format: ${chalk.blue("School")} / ${chalk.green("Term <number>")} / ${chalk.yellow("<Subject>")} / ${chalk.red("<Unit>")} / <Assignments|Finished>`)
-  let stuff = await shell.exec(`find ${getDirectoryPath("").slice(0,-1)} -type d  -not -path '*/\.*';`, {silent: true}).stdout.split("\n").slice(0,-1)
-  let formattedStuff = stuff.map(path => {
-    let tempPath = path.replace(homedir + "/Documents", "").substring(1).split("/")
-    tempPath.map((folder, index) => {
-      if(index == 0){
-        tempPath[index] = chalk.blue(folder)
-      }
-      if(index == 1) {
-        tempPath[index] = chalk.green(folder)
-      }
-      if(index == 2) {
-        tempPath[index] = chalk.yellow(folder)
-      }
-      if(index == 3) {
-        tempPath[index] = chalk.red(folder)
-      }
-    })
-    console.log(tempPath.join("/"))
-    return tempPath
-  })
-
+  let homedir = await os.homedir();
+  console.log(
+    `Format: ${chalk.blue("School")} / ${chalk.green(
+      "Term <number>"
+    )} / ${chalk.yellow("<Subject>")} / ${chalk.red(
+      "<Unit>"
+    )} / <Assignments|Finished>`
+  );
+  let stuff = await shell
+    .exec(
+      `find ${getDirectoryPath("").slice(
+        0,
+        -1
+      )} -type d  -not -path '*/\.*' | sort;`,
+      { silent: true }
+    )
+    .stdout.split("\n")
+    .slice(0, -1);
 }
 
 module.exports = {
