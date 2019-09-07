3d8103abb8b9c3876ffa6b6a2365d6d50d95554f
Rishi Kothari <itsrishikothari@gmail.com>
2019-09-07 17:28:17 -0400

    Finalizing stuff

--- bin/commands/notes/makeNote.js
+++ bin/commands/notes/makeNote.js
@@ -9,7 +9,7 @@ const shell = require("shelljs");
 const inquirer = require("inquirer");
 const copyDir = require("copy-dir");
 
-async function makeHomework(name) {
+async function makeNote(name) {
   inquirer.registerPrompt('datetime', require('inquirer-datepicker-prompt'))
   await verifyCmd("git");
   console.log(
@@ -19,28 +19,28 @@ async function makeHomework(name) {
   
   let path = await askForDirectory(3, "piece of note");
 
-  if (await verifyDirectory(`${path}/Note/${name}`, true)) {
+  if (await verifyDirectory(`${path}/Notes/${name}`, true)) {
     console.log(
       chalk.red("ERROR ") + chalk.blue("Note already exists on file.")
     );
     shell.exit(1);
   }
 
-  await makeDirectory(`${path}/Note/${name}`);
+  await makeDirectory(`${path}/Notes/${name}`);
   console.log("Note folder created.")
   let templateToCopy = await getGlobalPath(
     `/hoot-cli/templates/hoot-note`
   ).catch(err => console.log(err));
   await copyDir(
     templateToCopy,
-    getDirectoryPath(`${path}/Note/${name}`),
+    getDirectoryPath(`${path}/Notes/${name}`),
     {},
     function(err) {
       if (err) {
         console.error(err);
       } else {
         console.log("Copied " + "hoot-note" + " folder");
-        shell.cd(getDirectoryPath(`${path}/Note/${name}`));
+        shell.cd(getDirectoryPath(`${path}/Notes/${name}`));
         console.log("Changed directories to assignment");
         console.log("Initializing Git");
         if (shell.exec("git init").code !== 0) {
@@ -53,5 +53,5 @@ async function makeHomework(name) {
 }
 
 module.exports = {
-  makeHomework: makeHomework
+  makeNote: makeNote
 };
