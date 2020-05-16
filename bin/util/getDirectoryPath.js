f3638cf43a490121265e8e52adee6cb235c5036c
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-16 12:28:42 -0400

    Documentation and export rewrites

--- bin/util/getDirectoryPath.js
+++ bin/util/getDirectoryPath.js
@@ -1,10 +1,16 @@
 const os = require("os")
 
+/**
+ * A function for formatting paths relative to the ~/School folder.
+ *
+ * @param {string} subfolder The subfolder that will be parsed.
+ * @returns
+ */
 function getDirectoryPath(subfolder){
     let path = `${os.homedir()}/Documents/School/${subfolder}`
     return path
 }
 
 module.exports = {
-    getDirectoryPath : getDirectoryPath
+    getDirectoryPath
 }
