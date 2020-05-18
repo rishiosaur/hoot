6f8cb103cbe1cf5aead856d57f47a42e84e50f34
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-18 19:45:14 -0400

    Documentation

--- bin/util/messages.js
+++ bin/util/messages.js
@@ -1,5 +1,12 @@
 const chalk  = require('chalk')
 
+/**
+ * A function that curries together a status update.
+ *
+ * @param {*} color The color you would like the update to be.
+ * @param {*} type The type of update that will be deployed
+ * @returns A function that wil log a given message using the format to the console.
+ */
 function createMessage (color, type) {
     return function(message) {
         console.log(chalk.keyword(color)(type + ": ") + message)
