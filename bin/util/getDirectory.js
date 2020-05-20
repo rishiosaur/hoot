0ed74c82559edbcff1bb28cb63c7808c02023b3e
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-20 19:23:20 -0400

    Update documentation, rewrite exports

--- bin/util/getDirectory.js
+++ bin/util/getDirectory.js
@@ -2,17 +2,25 @@ const { readdirSync } = require("fs")
 const { getDirectoryPath } = require("./getDirectoryPath")
 const { verifyDirectory } = require("./verifyDirectory")
 
+/**
+ * A recursive method that returns the path to a given global directory, with respect to the School dir.
+ *
+ * @param {string | PathLike} subfolder The subfolder that is appended to the global directory
+ * @returns 
+ */
 function getDirectory (subfolder) {
     try {
         verifyDirectory(subfolder)
         return readdirSync(getDirectoryPath(subfolder), { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name);
+        .map(dirent => {
+            return dirent.name
+        });
     } catch(err) {
         return err
     }
 }
 
 module.exports = {
-    getDirectory : getDirectory
+    getDirectory
 }
